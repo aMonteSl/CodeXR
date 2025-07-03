c321ed2c55a5090d1e544a4fe73ec9dab46048c8
aMonteSl <a.montesl.2020@alumnos.urjc.es>
2025-07-03 18:58:05 +0200

    Version0.0.8

--- out/pythonEnv/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkAndSetupPythonEnvironment = exports.setupPythonEnvironment = exports.registerPythonEnvCommands = void 0;
-const commands_1 = require("./commands");
-Object.defineProperty(exports, "registerPythonEnvCommands", { enumerable: true, get: function () { return commands_1.registerPythonEnvCommands; } });
-Object.defineProperty(exports, "setupPythonEnvironment", { enumerable: true, get: function () { return commands_1.setupPythonEnvironment; } });
-Object.defineProperty(exports, "checkAndSetupPythonEnvironment", { enumerable: true, get: function () { return commands_1.checkAndSetupPythonEnvironment; } });
-//# sourceMappingURL=index.js.map
